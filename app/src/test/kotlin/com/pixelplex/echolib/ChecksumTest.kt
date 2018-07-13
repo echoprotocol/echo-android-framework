b5927ddb025a141b85ff66a8710b7202b30a9719
Dmitriy Bushuev <d.bushuev@pixelplex.by>
2018-07-13 16:38:34 +0300

    Added Vote and Checksum tests

---
+++ app/src/test/kotlin/com/pixelplex/echolib/ChecksumTest.kt
@@ -0,0 +1,25 @@
+package com.pixelplex.echolib
+
+import com.pixelplex.bitcoinj.Base58
+import com.pixelplex.echolib.support.Checksum
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * Test cases for [Checksum]
+ *
+ * @author Dmitriy Bushuev
+ */
+class ChecksumTest {
+
+    @Test
+    fun checksumLengthTest() {
+        val bytes = Base58.decode("JxF12TrwUP45BMd")
+
+        val hashSizeInBytes = 20
+        val hash = Checksum.calculateChecksum(bytes)
+
+        assertTrue(hash.size == hashSizeInBytes)
+    }
+
+}
