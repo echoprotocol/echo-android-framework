6ef7efc88f29ccde1b7f80b48613d9e6c92b0f95
Dmitriy Bushuev <d.bushuev@pixelplex.by>
2018-07-19 14:32:28 +0300

    Added base teset for framework

---
+++ echolib/src/test/kotlin/com/pixelplex/echolib/framework/EchoFrameworkTest.kt
@@ -0,0 +1,187 @@
+package com.pixelplex.echolib.framework
+
+import com.pixelplex.echolib.Callback
+import com.pixelplex.echolib.EchoFramework
+import com.pixelplex.echolib.exception.LocalException
+import com.pixelplex.echolib.model.Account
+import com.pixelplex.echolib.model.Balance
+import com.pixelplex.echolib.support.Api
+import com.pixelplex.echolib.support.Settings
+import com.pixelplex.echolib.support.concurrent.future.FutureTask
+import com.pixelplex.echolib.support.concurrent.future.wrapResult
+import com.pixelplex.echolib.support.fold
+import org.junit.Assert
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * Test cases for [EchoFramework]
+ *
+ * @author Dmitriy Bushuev
+ */
+class EchoFrameworkTest {
+
+    private fun initFramework() =
+        EchoFramework.create(
+            Settings.Configurator()
+                .setReturnOnMainThread(false)
+                .setApis(Api.DATABASE)
+                .configure()
+        )
+
+    @Test
+    fun connectTest() {
+        val framework = initFramework()
+
+        assertTrue(connect(framework) ?: false)
+    }
+
+    @Test
+    fun loginTest() {
+        val framework = initFramework()
+
+        val futureLogin = FutureTask<Account>()
+
+        if (connect(framework) == false) Assert.fail("Connection error")
+
+        framework.login("dimaty123", "P5JVzpPDitVodHMoj4zZspn7e8EYiDeoarkCEixS5tD3z",
+            object : Callback<Account> {
+                override fun onSuccess(result: Account) {
+                    futureLogin.setComplete(result)
+                }
+
+                override fun onError(error: LocalException) {
+                    futureLogin.setComplete(error)
+                }
+
+            })
+
+        val account = futureLogin.get()
+        assertTrue(account != null)
+
+        println(account.toString())
+    }
+
+    @Test
+    fun getAccountTest() {
+        val framework = initFramework()
+
+        val futureAccount = FutureTask<Account>()
+
+        if (connect(framework) == false) Assert.fail("Connection error")
+
+        framework.getAccount("dimaty123", object : Callback<Account> {
+            override fun onSuccess(result: Account) {
+                futureAccount.setComplete(result)
+            }
+
+            override fun onError(error: LocalException) {
+                futureAccount.setComplete(error)
+            }
+
+        })
+
+        val account = futureAccount.get()
+        assertTrue(account != null)
+    }
+
+    @Test
+    fun checkAccountReservedTest() {
+        val framework = initFramework()
+
+        val futureCheckReserved = FutureTask<Boolean>()
+
+        if (connect(framework) == false) Assert.fail("Connection error")
+
+        framework.checkAccountReserved("dimaty123", object : Callback<Boolean> {
+            override fun onSuccess(result: Boolean) {
+                futureCheckReserved.setComplete(result)
+            }
+
+            override fun onError(error: LocalException) {
+                futureCheckReserved.setComplete(error)
+            }
+
+        })
+
+        assertTrue(futureCheckReserved.get() ?: false)
+
+        val futureCheckAvailable = FutureTask<Boolean>()
+
+        framework.checkAccountReserved("edgewruferjd", object : Callback<Boolean> {
+            override fun onSuccess(result: Boolean) {
+                futureCheckAvailable.setComplete(result)
+            }
+
+            override fun onError(error: LocalException) {
+                futureCheckAvailable.setComplete(error)
+            }
+
+        })
+
+        assertFalse(futureCheckAvailable.get() ?: false)
+    }
+
+    @Test
+    fun getBalanceTest() {
+        val framework = initFramework()
+
+        val futureBalanceExistent = FutureTask<Balance>()
+
+        if (connect(framework) == false) Assert.fail("Connection error")
+
+        framework.getBalance("dimaty123", "1.3.0", object : Callback<Balance> {
+            override fun onSuccess(result: Balance) {
+                futureBalanceExistent.setComplete(result)
+            }
+
+            override fun onError(error: LocalException) {
+                futureBalanceExistent.setComplete(error)
+            }
+
+        })
+
+        assertTrue(futureBalanceExistent.get() != null)
+
+        val futureBalanceNonexistent = FutureTask<Balance>()
+
+        framework.getBalance("dimaty123", "ergergger", object : Callback<Balance> {
+            override fun onSuccess(result: Balance) {
+                futureBalanceNonexistent.setComplete(result)
+            }
+
+            override fun onError(error: LocalException) {
+                futureBalanceNonexistent.setComplete(error)
+            }
+
+        })
+
+        var balance: Balance? = null
+
+        futureBalanceNonexistent.wrapResult<Balance, Exception>().fold({ foundBalance ->
+            balance = foundBalance
+        }, {
+        })
+
+        assertTrue(balance == null)
+    }
+
+    private fun connect(framework: EchoFramework): Boolean? {
+        val futureConnect = FutureTask<Boolean>()
+
+        framework.start(object : Callback<Any> {
+            override fun onSuccess(result: Any) {
+                futureConnect.setComplete(true)
+            }
+
+            override fun onError(error: LocalException) {
+                futureConnect.setComplete(false)
+            }
+
+        })
+
+        return futureConnect.get()
+    }
+
+}
