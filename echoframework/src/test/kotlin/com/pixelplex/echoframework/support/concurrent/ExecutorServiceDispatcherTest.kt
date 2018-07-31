cccbd0a7fb00faa3adfaf8745114bbcebaa0a76e
Dmitriy Bushuev <d.bushuev@pixelplex.by>
2018-07-31 13:13:43 +0300

    Refactoring. Added thread dispatcher test

---
+++ echoframework/src/test/kotlin/com/pixelplex/echoframework/support/concurrent/ExecutorServiceDispatcherTest.kt
@@ -0,0 +1,35 @@
+package com.pixelplex.echoframework.support.concurrent
+
+import com.pixelplex.echoframework.support.concurrent.future.FutureTask
+import org.junit.Assert.assertNotEquals
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Test cases for [ExecutorServiceDispatcher]
+ *
+ * @author Dmitriy Bushuev
+ */
+class ExecutorServiceDispatcherTest {
+
+    private lateinit var dispatcher: ExecutorServiceDispatcher
+
+    @Before
+    fun setUp() {
+        dispatcher = ExecutorServiceDispatcher()
+    }
+
+    @Test
+    fun newThreadDispatchTest() {
+        val currentThreadId = Thread.currentThread().id
+
+        val dispatcherThreadIdFuture = FutureTask<Long>()
+
+        dispatcher.dispatch(Runnable {
+            dispatcherThreadIdFuture.setComplete(Thread.currentThread().id)
+        })
+
+        assertNotEquals(currentThreadId, dispatcherThreadIdFuture.get())
+    }
+
+}
