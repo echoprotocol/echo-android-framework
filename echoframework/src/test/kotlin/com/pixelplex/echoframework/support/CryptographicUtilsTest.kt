82de356917a83a8424c6ea4e24ea140e846b04c0
Dmitriy Bushuev <d.bushuev@pixelplex.by>
2018-08-03 13:25:31 +0300

    Added tests for cyptographic support methods

---
+++ echoframework/src/test/kotlin/com/pixelplex/echoframework/support/CryptographicUtilsTest.kt
@@ -0,0 +1,62 @@
+package com.pixelplex.echoframework.support
+
+import org.junit.Assert
+import org.junit.Test
+import org.spongycastle.crypto.InvalidCipherTextException
+
+/**
+ * Test cases for support cryptographic utility methods
+ *
+ * @author Dmitriy Bushuev
+ */
+class CryptographicUtilsTest {
+
+    private val shortMessage = "short"
+    private val longMessage = "longlonglonglonglonglonglonglonglonglonglonglonglonglon" +
+            "longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong" +
+            "longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong" +
+            "longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong" +
+            "longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong"
+
+    private val seed = "seed".toByteArray()
+
+    @Test
+    fun encryptShortAESTest() {
+        val encrypted = encryptAES(shortMessage.toByteArray(), seed)
+
+        Assert.assertNotNull(encrypted)
+    }
+
+    @Test
+    fun encryptLongAESTest() {
+        val encrypted = encryptAES(longMessage.toByteArray(), seed)
+
+        Assert.assertNotNull(encrypted)
+    }
+
+    @Test
+    fun decryptShortTest() {
+        val encrypted = encryptAES(shortMessage.toByteArray(), seed)
+
+        val decrypted = decryptAES(encrypted!!, seed)
+
+        Assert.assertEquals(String(decrypted!!), shortMessage)
+    }
+
+    @Test
+    fun decryptLongTest() {
+        val encrypted = encryptAES(longMessage.toByteArray(), seed)
+
+        val decrypted = decryptAES(encrypted!!, seed)
+
+        Assert.assertEquals(String(decrypted!!), longMessage)
+    }
+
+    @Test(expected = InvalidCipherTextException::class)
+    fun decryptErrorTest() {
+        val encrypted = encryptAES(longMessage.toByteArray(), "wrongSeed".toByteArray())
+
+        decryptAES(encrypted!!, seed)
+    }
+
+}
