d34a83ad0f79a1290009a9e00e7220d76bc954c2
d.bushuev <d.bushuev@pixelplex.io>
2018-08-10 11:13:10 +0300

    Subscription logic moved to corresponding facade

--- echoframework/src/main/kotlin/com/pixelplex/echoframework/service/internal/DatabaseApiServiceImpl.kt
+++ echoframework/src/main/kotlin/com/pixelplex/echoframework/service/internal/DatabaseApiServiceImpl.kt
@@ -1,11 +1,8 @@
 package com.pixelplex.echoframework.service.internal
 
-import com.pixelplex.echoframework.AccountListener
 import com.pixelplex.echoframework.Callback
 import com.pixelplex.echoframework.ILLEGAL_ID
-import com.pixelplex.echoframework.core.logger.internal.LoggerCoreComponent
 import com.pixelplex.echoframework.core.socket.SocketCoreComponent
-import com.pixelplex.echoframework.core.socket.SocketMessengerListener
 import com.pixelplex.echoframework.exception.LocalException
 import com.pixelplex.echoframework.model.*
 import com.pixelplex.echoframework.model.contract.ContractInfo
@@ -14,9 +11,10 @@ import com.pixelplex.echoframework.model.contract.ContractStruct
 import com.pixelplex.echoframework.model.network.Network
 import com.pixelplex.echoframework.model.socketoperations.*
 import com.pixelplex.echoframework.service.DatabaseApiService
-import com.pixelplex.echoframework.support.*
+import com.pixelplex.echoframework.support.Result
 import com.pixelplex.echoframework.support.concurrent.future.FutureTask
 import com.pixelplex.echoframework.support.concurrent.future.wrapResult
+import com.pixelplex.echoframework.support.dematerialize
 import java.util.concurrent.TimeUnit
 
 /**
@@ -32,15 +30,6 @@ class DatabaseApiServiceImpl(
 ) : DatabaseApiService {
 
     override var id: Int = ILLEGAL_ID
-
-    private var socketMessengerListener = SubscriptionListener()
-
-    @Volatile
-    private var subscribed = false
-
-    private val subscriptionManager: AccountSubscriptionManager by lazy {
-        AccountSubscriptionManagerImpl(network)
-    }
 
     override fun getFullAccounts(
         namesOrIds: List<String>,
@@ -207,99 +196,6 @@ class DatabaseApiServiceImpl(
         socketCoreComponent.emit(operation)
     }
 
-    override fun subscribeOnAccount(
-        nameOrId: String,
-        listener: AccountListener,
-        callback: Callback<Boolean>
-    ) {
-        synchronized(this) {
-            if (!subscribed) {
-                socketCoreComponent.on(socketMessengerListener)
-
-                this.subscribed = subscribeCallBlocking()
-                if (!subscribed) {
-                    callback.onError(LocalException("Subscription request error"))
-                }
-            }
-
-            if (!subscriptionManager.registered(nameOrId)) {
-                getAccountId(nameOrId)
-                    .flatMap { account -> getFullAccounts(listOf(account), true) }
-                    .value { accountsMap ->
-                        accountsMap.values.firstOrNull()?.account?.getObjectId()?.let { id ->
-                            subscriptionManager.registerListener(id, listener)
-                            callback.onSuccess(subscribed)
-                        }
-                    }
-                    .error { error ->
-                        LOGGER.log("Account finding error.", error)
-                        callback.onError(error)
-                    }
-            } else {
-                subscriptionManager.registerListener(nameOrId, listener)
-                callback.onSuccess(subscribed)
-            }
-        }
-    }
-
-    override fun unsubscribeFromAccount(nameOrId: String, callback: Callback<Boolean>) {
-        // if there are listeners registered with [nameOrId] - remove them
-        // else - request account by [nameOrId] and try remove listeners by account's id
-        if (!subscriptionManager.registered(nameOrId)) {
-            synchronized(this) {
-                if (!subscriptionManager.registered(nameOrId)) {
-                    getAccountId(nameOrId)
-                        .map { id -> subscriptionManager.removeListeners(id) }
-                        .value { existedListeners ->
-                            if (existedListeners != null) {
-                                callback.onSuccess(true)
-                            } else {
-                                LOGGER.log("No listeners found for this account")
-                                callback.onError(LocalException("No listeners found for this account"))
-                            }
-                        }
-                        .error { error ->
-                            LOGGER.log("Account finding error.", error)
-                            callback.onError(error)
-                        }
-                }
-            }
-        } else {
-            subscriptionManager.removeListeners(nameOrId)
-            callback.onSuccess(true)
-        }
-    }
-
-    private fun getAccountId(nameOrId: String): Result<LocalException, String> =
-        getFullAccounts(listOf(nameOrId), false)
-            .flatMap { accountsMap ->
-                accountsMap[nameOrId]?.account?.getObjectId()?.let { Result.Value(it) }
-                    ?: Result.Error(LocalException())
-            }
-            .mapError {
-                LocalException("Unable to find required account id for identifier = $nameOrId")
-            }
-
-    override fun unsubscribeAll(callback: Callback<Boolean>) {
-        synchronized(this) {
-            if (subscribed) {
-                cancelAllSubscriptions()
-                    .value { result ->
-                        subscribed = !result
-
-                        socketCoreComponent.off(socketMessengerListener)
-                        subscriptionManager.clear()
-                        callback.onSuccess(result)
-                    }
-                    .error { error ->
-                        callback.onError(error)
-                    }
-            } else {
-                callback.onSuccess(true)
-            }
-        }
-    }
-
     override fun callContractNoChangingState(
         contractId: String,
         registrarNameOrId: String,
@@ -412,96 +308,5 @@ class DatabaseApiServiceImpl(
         return future.wrapResult()
     }
 
-    private fun subscribeCallBlocking(): Boolean {
-        val futureResult = FutureTask<Boolean>()
-
-        val subscriptionOperation = createSubscriptionOperation(true, object : Callback<Any> {
-            override fun onSuccess(result: Any) {
-                futureResult.setComplete(true)
-            }
-
-            override fun onError(error: LocalException) {
-                futureResult.setComplete(error)
-            }
-
-        })
-
-        socketCoreComponent.emit(subscriptionOperation)
-
-        var result = false
-
-        futureResult.wrapResult<Exception, Boolean>(false)
-            .value {
-                result = it
-            }
-            .error { error ->
-                LOGGER.log("Subscription request error", error)
-                result = false
-            }
-
-        return result
-    }
-
-    private fun cancelAllSubscriptions(): Result<LocalException, Boolean> {
-        val future = FutureTask<Boolean>()
-        val cancelSubscriptionsOperation = CancelAllSubscriptionsSocketOperation(
-            id,
-            callId = socketCoreComponent.currentId,
-            callback = object : Callback<Any> {
-                override fun onSuccess(result: Any) {
-                    future.setComplete(true)
-                }
-
-                override fun onError(error: LocalException) {
-                    future.setComplete(error)
-                }
-            }
-        )
-        socketCoreComponent.emit(cancelSubscriptionsOperation)
-
-        return future.wrapResult(false)
-    }
-
-    private fun createSubscriptionOperation(clearFilter: Boolean, callback: Callback<Any>) =
-        SetSubscribeCallbackSocketOperation(
-            id,
-            clearFilter,
-            socketCoreComponent.currentId,
-            callback
-        )
-
-    private inner class SubscriptionListener : SocketMessengerListener {
-
-        override fun onEvent(event: String) {
-            // no need to process other events)
-            if (event.toJsonObject()?.get(DatabaseApiServiceImpl.METHOD_KEY)?.asString !=
-                DatabaseApiServiceImpl.NOTICE_METHOD_KEY
-            ) {
-                return
-            }
-
-            subscriptionManager.processEvent(event)
-        }
-
-        // implement failures
-        override fun onFailure(error: Throwable) {
-        }
-
-        override fun onConnected() {
-        }
-
-        override fun onDisconnected() {
-        }
-
-    }
-
-    companion object {
-        const val METHOD_KEY = "method"
-        private const val NOTICE_METHOD_KEY = "notice"
-
-        private val LOGGER = LoggerCoreComponent.create(DatabaseApiServiceImpl::class.java.name)
-    }
-
 }
-
 
