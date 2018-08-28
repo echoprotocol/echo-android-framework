eb3de5a2841fcaecb4bafda548df56acfb690ddd
d.bushuev <d.bushuev@pixelplex.io>
2018-08-28 12:20:27 +0300

    Checksum test improvement

--- echoframework/src/test/kotlin/com/pixelplex/echoframework/support/ChecksumTest.kt
+++ echoframework/src/test/kotlin/com/pixelplex/echoframework/support/ChecksumTest.kt
@@ -2,8 +2,10 @@ package com.pixelplex.echoframework.support
 
 import com.pixelplex.bitcoinj.Base58
 import com.pixelplex.echoframework.support.crypto.Checksum
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.spongycastle.util.encoders.Hex
 
 /**
  * Test cases for [Checksum]
@@ -14,12 +16,27 @@ class ChecksumTest {
 
     @Test
     fun checksumLengthTest() {
-        val bytes = Base58.decode("JxF12TrwUP45BMd")
-
+        val checksum = checksum("JxF12TrwUP45BMd")
         val hashSizeInBytes = Checksum.CHECKSUM_SIZE
-        val hash = Checksum.calculateChecksum(bytes)
 
-        assertTrue(hash.size == hashSizeInBytes)
+        assertTrue(checksum.size == hashSizeInBytes)
+    }
+
+    @Test
+    fun checksumValidationTest() {
+        val checksum = checksum("JxF12TrwUP45BMd")
+
+        assertEquals(VALID_CHECKSUM_HEX, Hex.toHexString(checksum))
+    }
+
+    private fun checksum(source: String): ByteArray {
+        val bytes = Base58.decode(source)
+
+        return Checksum.calculateChecksum(bytes)
+    }
+
+    companion object {
+        private const val VALID_CHECKSUM_HEX = "a830d7be"
     }
 
 }
